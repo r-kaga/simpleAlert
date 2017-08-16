0eafd62598e954b5930bc9475aade67ada7a7714
加賀谷諒 <ryokagaya0624@gmail.com>
2017-08-16 23:25:07 +0900

    Alert Call

--- simpleCustomAlert/ViewController.swift
+++ simpleCustomAlert/ViewController.swift
@@ -17,12 +17,22 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert(_ sender: Any) {
         
-//        DoubleAlert.show(self, title: "タイトル", message: "メッセージを\n表示しています") { action in
-//            switch action {
-//                case .Accept : print("accept!")
-//                case .Cancel : print("cancel!")
-//            }
-//        }
+        DoubleAlert.show(self, title: "タイトル", message: "メッセージを\n表示しています") { action in
+            switch action {
+                case .Accept : print("accept!")
+                case .Cancel : print("cancel!")
+            }
+        }
+        
+        SingleAlert.show(self, title: "タイトル", message: "メッセージを\n表示しています") { action in
+            print(action)
+            
+        }
+        
+        Alert.show(self, title: "Good Job!", message: "You Click The Button", buttonTitle: "OK") { action in
+            print("OK")
+        }
+
         
         Alert.show(self, title: "Good Job!", message: "You Click The Button", cancelTitle: "Cancel", acceptTitle: "OK") { action in
             print("OK")

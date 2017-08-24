9c3f5fc5ddfd1ac7ff71bae0ab9916ba4b346fa9
加賀谷諒 <ryokagaya0624@gmail.com>
2017-08-25 00:07:43 +0900

    AppSizeにアラートの大きさを定義

--- simpleCustomAlert/AppSize.swift
+++ simpleCustomAlert/AppSize.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 struct AppSize {
+    
     static var screenWidth: CGFloat { return UIScreen.main.bounds.width }
     static var screenHeight: CGFloat { return UIScreen.main.bounds.height }
     static var navigationBarHeight: CGFloat { return 44 }
@@ -16,6 +17,8 @@ struct AppSize {
     static var statusBarHeight: CGFloat { return UIApplication.shared.statusBarFrame.height }
     static var statusBarAndNavigationBarHeight: CGFloat { return statusBarHeight + navigationBarHeight }
     
+    static let contentViewRect: CGRect = CGRect(x: AppSize.screenWidth / 2, y: AppSize.screenHeight / 2,
+                                                width: AppSize.screenWidth / 1.2, height: AppSize.screenHeight / 2)
     
     static var window: CGRect {
         let app = UIApplication.shared.delegate as! AppDelegate
